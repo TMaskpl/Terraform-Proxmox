21ae94f9584353dc66a187838317821f8c1798aa
TMaskpl <75216446+TMaskpl@users.noreply.github.com>
2022-12-08 14:38:54 +0100

    Update create_vm.sh

--- create_vm.sh
+++ create_vm.sh
@@ -1,13 +1,12 @@
 # clone cloud image to new VM
 qm clone 9000 999 --name test-clone-cloud-init
 qm set 999 --sshkey id_rsa_cloudinit.pub 
-qm set 999 --ipconfig0 ip=10.20.222.88/24,gw=10.20.222.1
+qm set 999 --ipconfig0 ip=10.10.10.10/24,gw=10.10.10.1
 qm start 999
   
-# remove known host because SSH key changed
-ssh-keygen -f "/root/.ssh/known_hosts" -R "10.20.222.88"
-# ssh in
+ssh-keygen -f "/root/.ssh/known_hosts" -R "10.10.10.10"
+
 sleep 60 
-ssh -o StrictHostKeyChecking=no -i id_rsa_cloudinit ubuntu@10.20.222.88
+ssh -o StrictHostKeyChecking=no -i id_rsa_cloudinit ubuntu@10.10.10.10
 # stop and destroy VM
 # qm stop 999 && qm destroy 999
