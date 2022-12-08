124bad8e75a31dc5239714bfec3206a5e659c13e
TMaskpl <75216446+TMaskpl@users.noreply.github.com>
2022-12-08 14:37:27 +0100

    Update create_template.sh

--- create_template.sh
+++ create_template.sh
@@ -3,7 +3,7 @@ sudo apt install libguestfs-tools -y
 rm kinetic-server-cloudimg-amd64.img 
 wget https://cloud-images.ubuntu.com/kinetic/current/kinetic-server-cloudimg-amd64.img
 virt-customize -a kinetic-server-cloudimg-amd64.img --root-password password:newpassword1 --firstboot-install qemu-guest-agent
-qm create 9000 --name "ubuntu-2210-cloudinit-template" --memory 2048 --cores 2 --net0 virtio,bridge=vmbr20
+qm create 9000 --name "ubuntu-2210-cloudinit-template" --memory 2048 --cores 2 --net0 virtio,bridge=vmbr0
 qm importdisk 9000 kinetic-server-cloudimg-amd64.img local-lvm
 qm set 9000 --scsihw virtio-scsi-pci --scsi0 local-lvm:vm-9000-disk-0
 qm set 9000 --boot c --bootdisk scsi0
