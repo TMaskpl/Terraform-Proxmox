5772647be8f2e5557a9ccbd68ddf5d691c4fb290
TMaskpl <75216446+TMaskpl@users.noreply.github.com>
2023-03-29 14:06:47 +0200

    Update main.tf

--- Proxmox/main.tf
+++ Proxmox/main.tf
@@ -9,22 +9,21 @@ terraform {
 
 provider "proxmox" {
   # url is the hostname (FQDN if you have one) for the proxmox host you'd like to connect to to issue the commands. my proxmox host is 'prox-1u'. Add /api2/json at the end for the API
-  pm_api_url = "https://${IP_PVE}:8006/api2/json"
+  pm_api_url = "https://${var.IP_PVE}:8006/api2/json"
 
   # api token id is in the form of: <username>@pam!<tokenId>
-  pm_api_token_id = "${ID}"
+  pm_api_token_id = "${var.ID}"
 
   # this is the full secret wrapped in quotes. don't worry, I've already deleted this from my proxmox cluster by the time you read this post
-  pm_api_token_secret = "${TOKEN}"
+  pm_api_token_secret = "${var.TOKEN}"
 
   # leave tls_insecure set to true unless you have your proxmox SSL certificate situation fully sorted out (if you do, you will know)
   pm_tls_insecure = true
 }
-
 # resource is formatted to be "[type]" "[entity_name]" so in this case
 # we are looking to create a proxmox_vm_qemu entity named test_server
 resource "proxmox_vm_qemu" "test_server" {
-  count = 1 # just want 1 for now, set to 0 and apply to destroy VM
+  count = 0 # just want 1 for now, set to 0 and apply to destroy VM
   name = "tst-vm-${count.index + 1}" #count.index starts at 0, so + 1 means this VM will be named test-vm-1 in proxmox
 
   # this now reaches out to the vars file. I could've also used this var above in the pm_api_url setting but wanted to spell it out up there. target_node is different than api_url. target_node is which node hosts the template and thus also which node will host the new VM. it can be different than the host you use to communicate with the API. the variable contains the contents "prox-1u"
@@ -70,7 +69,7 @@ resource "proxmox_vm_qemu" "test_server" {
   # be 10.98.1.91 since count.index starts at 0. this is how you can create
   # multiple VMs and have an IP assigned to each (.91, .92, .93, etc.)
 
-  ipconfig0 = "ip=10.10.10.9${count.index + 1}/24,gw=10.10.10.1"
+  ipconfig0 = "ip=10.20.222.9${count.index + 1}/24,gw=10.20.222.1"
   
   # sshkeys set using variables. the variable contains the text of the key.
   sshkeys = <<EOF
